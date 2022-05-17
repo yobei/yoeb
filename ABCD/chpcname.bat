1e9eafc88e4124bcd597685f87cbae563a63e510
yobei <88903834+yobei@users.noreply.github.com>
2022-05-18 01:24:35 +0800

    Add files via upload

--- ABCD/chpcname.bat
+++ ABCD/chpcname.bat
@@ -1,18 +1,18 @@
 
 @echo off
 
-for /f "delims=" %%a in ('call readini.bat /s OutputVar /i rob "C:\Users\Administrator\Desktop\iot\setnb.ini"') do (
+for /f "delims=" %%a in ('call readini.bat /s OutputVar /i rob "setnb.ini"') do (
     set val=%%a
 )
 
-echo %val%%computername%
 
 set name=%val%%computername%
 set srvname=%val%%computername%
-reg add ¡°HKEY_LOCAL_MACHINESystemCurrentControlSetControlComputerNameActiveComputerName¡± /v ComputerName /t reg_sz /d %name% /f
-reg add ¡°HKEY_LOCAL_MACHINESystemCurrentControlSetServicesTcpipParameters¡± /v ¡°NV Hostname¡± /t reg_sz /d %name% /f
-reg add ¡°HKEY_LOCAL_MACHINESystemCurrentControlSetServicesTcpipParameters¡± /v Hostname /t reg_sz /d %name% /f
-reg add ¡°HKEY_LOCAL_MACHINESYSTEMControlSet001servicesLanmanServerParameters¡± /v srvcomment  /t reg_sz /d %srvname% /f
+REG ADD HKLM\System\CurrentControlSet\Control\ComputerName\ActiveComputerName /v "ComputerName" /t reg_sz /d %name% /f
 
+REG ADD HKLM\System\CurrentControlSet\Services\Tcpip\Parameters    /v "Hostname"    /t reg_sz    /d %name% /f
+REG ADD HKLM\SYSTEM\ControlSet001\services\LanmanServer\Parameters /v "srvcomment"  /t reg_sz /d %srvname% /f
 
-pause
+REG ADD HKLM\System\CurrentControlSet\Services\Tcpip\Parameters    /v "NV Hostname" /t reg_sz /d %name% /f
+REG ADD HKLM\SYSTEM\ControlSet001\Services\Tcpip\Parameters        /v "NV Hostname" /t REG_SZ /d %name% /f
+
