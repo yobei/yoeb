f1cce1f5305e3775849f8f53ec02fa7c41a895e3
yobei <88903834+yobei@users.noreply.github.com>
2022-05-18 15:56:35 +0800

    Add files via upload

--- ABCD/chpcname.bat
+++ ABCD/chpcname.bat
@@ -7,11 +7,5 @@ for /f "delims=" %%a in ('call readini.bat /s OutputVar /i nob "C:\Users\Adminis
 
 
 set name=%val%%computername:~0,12%
-set srvname=%val%%computername:~0,12%
-REG ADD HKLM\System\CurrentControlSet\Control\ComputerName\ActiveComputerName /v "ComputerName" /t reg_sz /d %name% /f
 
-REG ADD HKLM\System\CurrentControlSet\Services\Tcpip\Parameters    /v "Hostname"    /t reg_sz    /d %name% /f
-REG ADD HKLM\SYSTEM\ControlSet001\services\LanmanServer\Parameters /v "srvcomment"  /t reg_sz /d %srvname% /f
-
-REG ADD HKLM\System\CurrentControlSet\Services\Tcpip\Parameters    /v "NV Hostname" /t reg_sz /d %name% /f
-REG ADD HKLM\SYSTEM\ControlSet001\Services\Tcpip\Parameters        /v "NV Hostname" /t REG_SZ /d %name% /f
+wmic.exe ComputerSystem Where Name="%ComputerName%" Rename Name="%name%"
