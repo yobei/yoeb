455394a95fdee0a6b18784a064a682ae5a8da262
yobei <88903834+yobei@users.noreply.github.com>
2022-05-19 10:41:40 +0800

    Add files via upload

--- ABCD/脚本/自动任务.lua
+++ ABCD/脚本/自动任务.lua
@@ -7,7 +7,7 @@ do
 end
 
 AddTaskWork("自动跑图",AutoTask)
-AddTaskWork("自动升级",AutoShengJi,false,true)--升级到设置的刷异界的等级1
+AddTaskWork("自动升级",AutoShengJi,false,true)--升级到设置的刷异界的等级
 AddTaskWork("刷白袍",AutoShuaBaiPao,false,true)
 AddTaskWork("自动迷宫",AutoShiLian)
 AddTaskWork("异界任务",AutoYiJieTask)
