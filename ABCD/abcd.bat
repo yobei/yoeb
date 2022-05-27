ad606211ba17d4ecf7e946d2e5368c072b56322a
yobei <88903834+yobei@users.noreply.github.com>
2022-05-28 02:08:33 +0800

    Update abcd.bat

--- ABCD/abcd.bat
+++ ABCD/abcd.bat
@@ -4,4 +4,4 @@ PUSHD %~DP0 & cd /d "%~dp0"
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
 :runas
 
-start "" "C:\Users\Administrator\Desktop\yoeb-main\ABCD\ABCD.exe"
+start "" "C:\Users\Administrator\Desktop\ABCD\abcdiot.exe"
