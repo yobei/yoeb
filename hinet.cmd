7a134ea976c8cc271b54ccd57e7ee806914de6f5
yobei <88903834+yobei@users.noreply.github.com>
2022-05-28 16:40:01 +0800

    Add files via upload

--- hinet.cmd
+++ hinet.cmd
@@ -1,6 +1,1 @@
-
-PUSHD %~DP0 & cd /d "%~dp0"
-%1 %2
-mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
-:runas
-rasdial å¯¬é »é€£ç·š 76426119@hinet.net  dxezgitm
+rasdial ¿í´øÁ¬½Ó 76426119@hinet.net dxezgitm
