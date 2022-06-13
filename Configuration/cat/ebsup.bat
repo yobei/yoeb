c5abff80bf556951f688519f5a76f6e1b97d5b6e
yobei <88903834+yobei@users.noreply.github.com>
2022-06-13 23:45:14 +0800

    Add files via upload

--- Configuration/cat/ebsup.bat
+++ Configuration/cat/ebsup.bat
@@ -1,2 +1,1 @@
-start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\OCC1\J=J+5.exe"
-start "" "C:\Users\Administrator\Desktop\yoeb-main\ebpro.exe"
+start "" "C:\Users\Administrator\Desktop\yoeb-main\ebpro.bat"
