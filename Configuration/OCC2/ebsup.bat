45b3b4d04aa5a6a15096e8443e5c1bdaf6ad266c
yobei <88903834+yobei@users.noreply.github.com>
2022-06-13 21:40:56 +0800

    Update ebsup.bat

--- Configuration/OCC2/ebsup.bat
+++ Configuration/OCC2/ebsup.bat
@@ -1,2 +1,2 @@
-start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\OCC2\J=J5.exe"
+start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\OCC2\J=J+5.exe"
 start "" "C:\Users\Administrator\Desktop\yoeb-main\ebpro.exe"
