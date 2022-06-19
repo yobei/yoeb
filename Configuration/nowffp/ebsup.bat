da990a5be9999114fd2a297b177c457424f1494a
yobei <88903834+yobei@users.noreply.github.com>
2022-06-19 18:54:48 +0800

    Update ebsup.bat

--- Configuration/nowffp/ebsup.bat
+++ Configuration/nowffp/ebsup.bat
@@ -1,1 +1,1 @@
-start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\nowfpp\W=W345T=C+QWERT.exe"
+start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\nowffp\W=W345T=C+QWERT.exe"
