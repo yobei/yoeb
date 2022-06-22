423875a74e288e4e78415c8da5f2069372d66b2d
yobei <88903834+yobei@users.noreply.github.com>
2022-06-22 17:50:29 +0800

    Update ebsup.bat

--- Configuration/bigfpp/ebsup.bat
+++ Configuration/bigfpp/ebsup.bat
@@ -1,3 +1,3 @@
-start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\bigffpp\W=W345T=C+QWERT.exe"
+start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\bigfpp\W=W345T=C+QWERT.exe"
 
 start "" "C:\Users\Administrator\Desktop\yoeb-main\Configuration\bigfpp\R=Rhide1s.exe"
