b0de2b0dde9e4189e4b4b5b0b75f66120d1cd688
yobei <88903834+yobei@users.noreply.github.com>
2023-04-13 13:45:34 +0800

    Add files via upload

--- ABCD/脚本/lightning.lua
+++ ABCD/脚本/lightning.lua
@@ -11,16 +11,10 @@ g_newRoleJob="游侠"
 --升华职业 勇士、暴徒、酋长、侠客、锐眼、追猎者、秘术家、元素使、召唤师、处刑者、卫士、冠军、判官、圣宗、守护者、暗影大师、欺诈师、破坏者、升华使徒、
 g_shengHuaJob="侠客"
 --设置使用的武器 支持(盾}箭袋}爪}匕首|法杖|单手剑|细剑|单手斧|单手锤|符文匕首|短杖|战杖|弓|长杖|双手剑|双手斧|双手锤) 如果需要用两样中间用|分开
-SetUseWeapon("匕首|盾")
+SetUseWeapon("双手剑")
 --设置自己用的装备类型 
-SetUseZhuangBeiTypeData("Dex|DexInt|StrDex","衣服")
-SetUseZhuangBeiTypeData("Dex","头盔")
-SetUseZhuangBeiTypeData("Str|StrInt|Int","手套")
-SetUseZhuangBeiTypeData("Dex","鞋子")
-SetUseZhuangBeiTypeData("StrInt","项链")
-SetUseZhuangBeiTypeData("Str|StrInt","盾")
 
-g_attackDis=20				--攻击距离
+g_attackDis=50				--攻击距离
 g_duobiHpVal=0.2
 
 --SetNeedSkillLineData(val,str,invalidLv,pos)--设置换技能宝石数据 
@@ -30,37 +24,34 @@ g_duobiHpVal=0.2
 --pos=指定位置 nil为自动选择 2为衣服 3为主武器 4为副武器 5为头盔 6为项链 7为左戒指 8为右戒指 9为护手 10为鞋子 11为腰带
 --nType 有效场景 nil为无论何时都有效 0为没在刷异界才有效 1为只在刷异界才有效
 
-SetNeedSkillLineData(10,"燃燒箭矢,nil|穿透輔助,nil",2,nil,nil)
-SetNeedSkillLineData(60,"元素淨化,nil|受傷時施放輔助,nil|信念浪湧,nil",nil,9,nil)
-SetNeedSkillLineData(70,"鋼筋鐵骨,nil|先祖衛士,nil|持續時間延長輔助,nil",nil,4,nil)
-SetNeedSkillLineData(80,"冰霜之捷,nil|盜獵者印記,nil|擊中時印記輔助,nil",nil,nil,nil)
-SetNeedSkillLineData(90,"凋零之步,nil|召喚寒冰魔像,nil",nil,nil,nil)
-SetNeedSkillLineData(99,"爆裂陷阱,nil|迅速組裝輔助,nil|機率中毒輔助,nil",18,nil,nil)
-SetNeedSkillLineData(100,"閃電打擊,nil|夜刃輔助,nil|附加冰冷傷害輔助,nil|霜咬輔助,nil|附加閃電傷害輔助,nil|啟發輔助,nil|快速攻擊輔助,nil|機率中毒輔助,nil",nil,nil,nil)
+SetNeedSkillLineData(96,"旋渦,nil|無邊異能輔助,nil|寒冰刺骨輔助,nil|增加範圍輔助,nil",nil,10,nil)
+SetNeedSkillLineData(97,"刺客印記,nil|擊中時印記輔助,nil|凋零之步,nil",nil,9,nil)
+SetNeedSkillLineData(98,"受傷時施放輔助,nil|熔岩護盾,nil|持續時間延長輔助,nil",nil,5,nil)
+SetNeedSkillLineData(99,"啟蒙,nil|優雅,nil|精準,nil|憎恨,nil|冰霜之捷,nil|極地裝甲,nil",nil,2,nil)
+SetNeedSkillLineData(100,"瓦爾．閃電打擊,nil|多重打擊輔助,nil|啟發輔助,nil|元素攻擊傷害輔助,nil|三體輔助,nil|增加暴擊率輔助,nil",nil,3,nil)
+
 
 --添加攻击技能 name=技能名 className=技能类名 noLine=不在直线就能攻击 挑选攻击技能会从上到下寻找，要把厉害的技能加在前面
-
-AddAttackSkillData("閃電打擊","Spectral Helix")
-AddAttackSkillData("爆裂陷阱","Explosive Trap")
-AddAttackSkillData("燃燒箭矢","Burning Arrow")
+AddAttackSkillData("瓦爾．閃電打擊","Vaal Lightning Strike",false,50,10)
+AddAttackSkillData("閃電打擊","Lightning Strike",false,50,0)--添加攻击技能
 AddAttackSkillData("普通攻击","melee")
 
 --SetSkillLimitMaxLv(name,className,maxLv)--设置技能宝石最大等级 name=技能宝石物品名 className=技能宝石物品类名 maxLv=限制的最大等级
 SetSkillLimitMaxLv("受傷時施放輔助",nil,1)
-SetSkillLimitMaxLv("信念浪湧",nil,1)
-
+SetSkillLimitMaxLv("熔岩護盾",nil,10)
 
 -- SetYiJieZhaoHuanLingTiData(mapClassName,name,className)--设置异界时召唤的灵体数据 mapClassName=剧情图类名 name=灵体名 className=灵体类名
 
-SetNeedFlaskData(1,"生命药剂","nil")
-SetNeedFlaskData(2,"生命药剂","nil")
-SetNeedFlaskData(3,"水银药剂","nil")
-SetNeedFlaskData(4,"魔力药剂","nil")
-SetNeedFlaskData(5,"魔力药剂","nil")
+--SetNeedFlaskData(pos,flask,modsData,minLv)--设置药瓶摆放
+SetNeedFlaskData(1,"生命药剂","熱情洋溢的,FlaskPartialInstantRecovery3|密封之,FlaskBleedCorruptingBloodImmunity1",90)
+SetNeedFlaskData(2,"魔力药剂","耐久的,FlaskEffectNotRemovedOnFullMana1|奧術師之FlaskBuffReducedManaCostWhileHealing4",90)
+SetNeedFlaskData(3,"宝钻药剂","nil")
+SetNeedFlaskData(4,"紫晶药剂","nil")
+SetNeedFlaskData(5,"水银药剂","nil")
 
 
 g_addHpVal=0.7--HP低于多少吃红药
-g_addMpVal=0.2--MP低于多少吃蓝药
+g_addMpVal=0.5--MP低于多少吃蓝药
 
 ---------------------------------------换装设置
 --SetAtuoChangeEquipData(job,pos,name,val,yijie) 计算方式如下 定义过的属性值乘以比重值  哪样装备高就会用哪样
@@ -96,9 +87,8 @@ SetAtuoChangeEquipData("游侠","匕首","Local_critical_strike_chance_%",3,nil)
 SetAtuoChangeEquipData("游侠","盾","护甲",1,nil)
 
 --游侠--孔色
-SetZhengTiColorVarData(2,1,4,1,nil,700,true)
-SetZhengTiColorVarData(2,1,5,0,nil,660,true)
-SetZhengTiColorVarData(2,0,5,1,nil,630,true)
+SetZhengTiColorVarData(2,1,4,1,nil,650,true)
+SetZhengTiColorVarData(2,1,5,0,nil,630,true)
 SetZhengTiColorVarData(2,1,4,0,nil,600,true)
 SetZhengTiColorVarData(2,0,4,1,nil,580,true)
 SetZhengTiColorVarData(2,0,5,0,nil,550,true)
@@ -139,16 +129,16 @@ SetZhengTiColorVarData(10,0,1,0,nil,100)
 
 
 
-AddShengJiZhuangBeiGoodsData("点金石","Metadata/Items/Currency/CurrencyUpgradeToRare",0,50,0)--主线生效 超过50级就用点金点白色装备
-AddShengJiZhuangBeiGoodsData("富豪石","Metadata/Items/Currency/CurrencyUpgradeMagicToRare",1,50,0)--主线生效 超过50级就用富豪点蓝色装备
+--AddShengJiZhuangBeiGoodsData("点金石","Metadata/Items/Currency/CurrencyUpgradeToRare",0,30,0)--主线生效 超过50级就用点金点白色装备
+--AddShengJiZhuangBeiGoodsData("富豪石","Metadata/Items/Currency/CurrencyUpgradeMagicToRare",1,30,0)--主线生效 超过50级就用富豪点蓝色装备
 
 --掉落率+稀有率 异界时才有效
 --SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|法杖|盾","base_item_found_rarity_+%",0.5,2)--基礎物品掉落品質 +%
 --SetAtuoChangeEquipData("暗影|野蛮人|决斗者|女巫|游侠|圣堂武僧","戒指|项链|腰带|鞋子|手套|衣服|头盔|法杖|盾","base_item_found_quantity_+%",1.5,2)--物品掉落數量增加 %
 
 --高级换装设置
-SetGaoJiHuanZhuangData("游侠","简易之袍","Metadata/Items/Armours/BodyArmours/BodyInt1","无尽之衣","Tabula Rasa")
-SetGaoJiHuanZhuangData("游侠","金缕帽","Metadata/Items/Armours/Helmets/HelmetDex1","金缕帽","Goldrim")
+
+SetShopNotBuyGoods(瓦爾．閃電打擊,nil)--设置不去商店里买的东西
 
 --SetTaskShengJiData(taskClassName,taskIndex,shengjiMapClassName,needLv)--设置跑图时升级 taskClassName=任务类名 taskIndex=任务索引 nil为忽略，只匹配任务类名 shengjiMapClassName=要刷的地图类名 needLv=升级到多少级为止
 SetTaskShengJiData("a5q7",nil,"1_5_5",46,true)--做 到奇塔弗的受难 的时候刷纯净圣地到50级再进行下一任务
@@ -158,6 +148,11 @@ SetTaskMiGongData("a8q7",nil,1)--大于等于a9q1级 做迷宫1
 SetTaskMiGongData("a10q1",nil,2)--大于等于a10q1 做迷宫2
 SetTaskMiGongDataByLv(80,3)--大于等于80级 做迷宫3
 
+g_openQlkMaxMapTianFuCnt=nil--数值型 满多少天赋后不开奇拉克任务nil为一直开
+g_useMinLvMapTianFuCnt=70--地图天赋满多少点后 就从低阶地图开始取
+SetYiJieShuaTuModeByMapCnt(60,nil,2)--)--根据仓库里的地图数量设置异界刷图模式 可添加多个 他是从地图数量多到少来判断的
+SetOpenMapMasterData(3)
+SetUseBuffSkill("傲貓祝福","Aspect_of_the_Cat")--
 
 SetChangeLimitCnt("武器|衣服|头盔|手套|鞋子",2)
 SetChangeLimitCnt("腰带",3)
@@ -165,5 +160,6 @@ SetChangeLimitCnt("项链|戒指",4)
 
 
 --SetNeedAddTianFu(tfStr)--设置天赋加点 
-SetNeedAddTianFu("游侠=evasion731-閃避和生命|dexterity988-敏捷|dexterity986-敏捷|dexterity984-敏捷|dexterity985-敏捷|dexterity861-敏捷|weapon_elemental_damage2163-武器元素傷害|weapon_elemental_damage2161-武器元素傷害，異常狀態機率|weapon_elemental_damage2162-武器元素傷害，異常狀態機率|weapon_elemental_damage_notable2165-太古之力|finesse993-嫻熟|reduced_mana1480-魔力和藥劑效果|mana1478-原始精神|dexterity873-敏捷|dexterity858-敏捷|dexterity968-敏捷|weapon_cold_damage212-武器冰冷傷害|weapon_cold_damage211-武器冰冷傷害|ice_bite433-刺骨寒冰|mastery_cold61-冰冷專精-13267|stun_recovery543-生命和避免暈眩|avoid_stun553-橡木之心|dexterity877-敏捷|might772-壯漢|savant937-原野智慧|life1167-生命和避免元素異常狀態|life1156-生命和避免元素異常狀態|life1120-生命和避免元素異常狀態|thick_skin1157-強化之膚|mastery_life133-生命專精-47642|bow_critical_strike_chance675-投射物攻擊暴擊率|bow_critical_strike_chance674-投射物攻擊暴擊率|heartpierce676-致命鋒芒|one_handed_damage636-武器元素傷害|shield_mastery440-自然合一|one_handed_damage637-武器元素傷害|claws_of_the_pride489-直覺|one_handed_damage1050-攻擊傷害|weapon_artistry620-舞刃宗師|one_handed_attack_speed1027-攻擊傷害|attack_damage_notable2120-山貓之靈|dexterity862-敏捷|life1216-生命|life1412-生命|alchemist532-藥草學|mastery_life146-生命專精-64381|dexterity864-敏捷|critical_strike_chance625-暴擊率|critical_strike_chance624-暴擊率|heartseeker626-刺心者|dexterity866-敏捷|dexterity865-敏捷|dexterity856-敏捷|might770-無畏|evasion1692-閃避與元素抗性|evasion_resists_notable1691-死裡逃生|dexterity872-敏捷|weapon_elemental_damage1263-武器元素傷害|weapon_elemental_damage1264-武器元素傷害|weapon_ele_notable1700-自然之力|mastery_elemental99-元素專精-30502|dexterity860-敏捷|dagger_damage2060-匕首傷害和移動速度|dagger_mobility2533-匕首的傷害和移動速度|from_the_shadows471-影刃|mastery_dagger78-匕首專精-44869|dexterity867-敏捷|intelligence927-智慧|intelligence926-智慧|dagger_damage264-匕首傷害|dagger_damage_and_critical_strike_chance465-匕首暴擊率和暴擊加成|dagger_damage_and_critical_strike_multiplier262-匕首暴擊率和加成|dagger_damage_and_critical_strike_multiplier263-匕首暴擊率和加成|dagger_global_crit_notable2527-背刺|dagger_damage_and_attack_speed466-匕首傷害和攻擊速度|dagger_damage_and_attack_speed467-匕首傷害和攻擊速度|dagger_damage_and_attack_speed468-匕首傷害和攻擊速度|flaying266-剝皮刃|accuracy587-命中和暴擊率|accuracy586-命中和暴擊率|deadeye588-潛意識|life1415-生命|life_life_leech1629-嗜血者|life1413-生命|dexterity857-敏捷|dexterity991-敏捷|intelligence962-智慧|intelligence953-智慧|spell_power1227-冷酷計算|intelligence964-智慧|dagger_crit2532-匕首的暴擊率|dagger_damage_and_critical_strike_chance258-匕首暴擊率和偷取|nightstalker261-夜襲|dagger_damage_and_critical_strike_chance257-匕首暴擊加成|mastery_dagger77-匕首專精-63223|mental_acuity1046-詭計|critical_strike_chance1019-暴擊率|assassination1239-刺殺|")
-SetNeedAddTianFu("游侠升华=AscendancyRaider5-閃避、猛攻效果|AscendancyRaider6-極速進攻|AscendancyRaider7-閃避、猛攻效果|AscendancyRaider8-追獵使徒|AscendancyRaider3-閃避、狂怒球持續時間|AscendancyRaider2-盜獵者之途|") 
+
+SetNeedAddTianFu("游侠=attack_speed568-投射物傷害和命中|dexterity990-敏捷|dexterity992-敏捷|dexterity995-敏捷|intelligence957-智慧|aura_area_of_effect1203-光環效果範圍|reduced_mana_reservation1199-保留效用|aura_effect_reservation_cost_notable1558-魅力|mastery_reservation208-保留專精-61097|dexterity989-敏捷|dexterity872-敏捷|weapon_elemental_damage1263-武器元素傷害|weapon_elemental_damage1264-武器元素傷害|weapon_ele_notable1700-自然之力|mastery_elemental99-元素專精-30502|life1119-閃避和生命|life1118-閃避和生命|life1117-死亡通緝|mastery_life130-閃避專精-57074|dexterity976-敏捷|attack_damage_notable2120-山貓之靈|evasion1090-閃避和元素抗性|evasion_resists_notable1691-死裡逃生|dexterity969-敏捷|dexterity870-敏捷|claws_of_the_pride489-直覺|one_handed_damage637-武器元素傷害|shield_mastery440-自然合一|one_handed_damage636-武器元素傷害|finesse993-嫻熟|reduced_mana1480-魔力和藥劑效果|mana1478-原始精神|stun_recovery543-生命和避免暈眩|avoid_stun553-橡木之心|dual_wield_damage2125-移動速度和法術壓抑|attack_move_speed_notable1792-疾步|life1412-生命|alchemist532-藥草學|life1216-生命|dexterity862-敏捷|dexterity864-敏捷|accuracy582-命中與攻擊速度|accuracy589-命中和攻擊速度|deadeye590-心眼|mastery_accuracy3-命中專精-51147|evasion_per_frenzy_charge535-每顆狂怒球閃避|maximum_frenzy_charges525-激情|critical_strike_chance625-暴擊率|critical_strike_chance624-暴擊率|heartseeker626-刺心者|dexterity1986-敏捷|jewel_slot1960-基礎珠寶插槽|dexterity861-敏捷|weapon_elemental_damage2163-武器元素傷害|weapon_elemental_damage2161-武器元素傷害，異常狀態機率|weapon_elemental_damage2162-武器元素傷害，異常狀態機率|weapon_elemental_damage_notable2165-太古之力|dexterity873-敏捷|dexterity858-敏捷|dexterity968-敏捷|weapon_cold_damage212-武器冰冷傷害|weapon_cold_damage211-武器冰冷傷害|ice_bite433-刺骨寒冰|dexterity835-敏捷|dexterity844-敏捷|dexterity840-敏捷|mana_on_kill1476-魔力和光環範圍效果|mana_on_kill_notable1704-狂歡|mastery_mana171-魔力專精-12119|dexterity838-敏捷|dexterity1990_-敏捷|maximum_frenzy_charges527-野性之心|frenzy_charges2419-每顆狂怒球閃避|frenzy_charges2418-移動速度|frenzy_charges2420-每顆狂怒球攻擊速度|frenzy_charges_notable2421-屠宰者之使徒|strength1005-力量|life1213-生命|golem's_blood1088-魔像血統 |armour_and_evasion1454-護甲和閃避|leather_and_steel1458-迅影秘術|iron_reflexes1137-霸體|strength832-力量|strength802-力量|strength794-力量|strikes_damage2731_-近戰傷害|strikes_speed2734-近戰傷害和擴散範圍效果|strikes_speed2735-近戰傷害和擴散範圍效果|blight_special_notable6-部落之怒|strength778-力量|savant954-靜修|aura_effect1555-光環效果|aura_area_of_effect1202-光環效果範圍|general_aura_notable1791-起源|aura_banner2426-保留效用|life1167-生命和避免元素異常狀態|life1156-生命和避免元素異常狀態|life1120-生命和避免元素異常狀態|thick_skin1157-強化之膚|mastery_reservation207-保留專精-28638|sword_damage1069-劍的暴擊率|sword_attack_speed1931-劍暴擊加成|sword_attack_speed1930-劍暴擊加成|sword_damage_speed_evasion_notable1932-巧刃|mastery_life138-生命專精-47642|mastery_evasion115-護甲和閃避專精-62285|") 
+SetNeedAddTianFu("游侠升华=AscendancyRaider5-閃避、猛攻效果|AscendancyRaider6-極速進攻|AscendancyRaider7-閃避、猛攻效果|AscendancyRaider8-追獵使徒|AscendancyRaider3-閃避、狂怒球持續時間|AscendancyRaider2-盜獵者之途|AscendancyRaider1-閃避、狂怒球持續時間|AscendancyRaider4-殺戮使徒|") 
