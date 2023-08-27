364adf87f1768599f2e0768f8962b23f49d461ab
xlgj79718 <34760265+xlgj79718@users.noreply.github.com>
2023-08-28 05:16:30 +0800

    Add files via upload

--- ABCD/脚本/testice1.lua
+++ ABCD/脚本/testice1.lua
@@ -61,10 +61,10 @@ g_duobiHpVal=0.2
 g_yiJieLv=70				--够了多少级才去刷异界
 g_checkSellMapCnt=40		--地图超过这个数量才会检测出售地图
 g_destroyNoUseMap=false		--销毁上面设置不使用的地图 nil或false为不销毁
-g_mapUseFuHaoLv=76			--大于等于多少级对蓝图使用富豪石 nil为永远不使用
+g_mapUseFuHaoLv=75			--大于等于多少级对蓝图使用富豪石 nil为永远不使用
 g_mapUseZengFuLv=nil			--大于等于多少级对蓝图使用增幅石 nil为永远不使用
-g_mapUseDianJingLv=76		--大于等于多少级对白图使用点金石 nil为永远不使用
-g_mapUseTuiBianLv=72		--大于等于多少级对白图使用蜕变石 nil为永远不使用
+g_mapUseDianJingLv=75		--大于等于多少级对白图使用点金石 nil为永远不使用
+g_mapUseTuiBianLv=60		--大于等于多少级对白图使用蜕变石 nil为永远不使用
 g_mapUseJiHuiLv=nil			--大于等于多少级对白图使用机会石 nil为永远不使用
 g_mapUseWaErLv=83			--大于等于多少级对白、蓝地图使用瓦尔宝珠 nil为永远不使用
 g_mapUseDingZiLv=nil			--大于等于多少级使用制图钉 nil为永远不使用
@@ -140,5 +140,5 @@ SetNeedAddTianFu("异界地图天赋=atlas_boss_adjacent_maps_2-相鄰地圖掉落機率|atlas
 
 
 --SetNeedAddTianFu(tfStr)--设置天赋加点 
-SetNeedAddTianFu("游侠=evasion731-閃避和生命|dexterity988-敏捷|dexterity986-敏捷|dexterity984-敏捷|finesse993-嫻熟|reduced_mana1480-魔力和藥劑效果|mana1478-原始精神|one_handed_damage636-武器元素傷害|shield_mastery440-自然合一|one_handed_damage637-武器元素傷害|claws_of_the_pride489-直覺|dexterity985-敏捷|cold_damage2025-冰冷傷害|cold_damage2026-冰冷傷害和冰冷異常狀態效果|cold_damage_notable2027-閃現冰凍|dexterity861-敏捷|weapon_elemental_damage2163-武器元素傷害|weapon_elemental_damage2161-武器元素傷害，異常狀態機率|weapon_elemental_damage2162-武器元素傷害，異常狀態機率|weapon_elemental_damage_notable2165-太古之力|mastery_elemental96-元素專精-17942|dexterity862-敏捷|dexterity864-敏捷|dexterity866-敏捷|dexterity865-敏捷|dexterity856-敏捷|dexterity855-敏捷|dexterity848-敏捷|intelligence957-智慧|aura_area_of_effect1203-光環效果範圍|reduced_mana_reservation1199-保留效用|aura_effect_reservation_cost_notable1558-魅力|life1116-閃避和生命|life1115-閃避和生命|life1117-死亡通緝|mastery_life130-閃避專精-57074|strength815-力量|dexterity981-敏捷|agility965-和諧之體|life703-生命|fitness617-血液抽取|life1220-生命|mental_acuity1046-詭計|intelligence964-智慧|dagger_crit2532-匕首的暴擊率|dagger_damage_and_critical_strike_chance258-匕首暴擊率和偷取|nightstalker261-夜襲|dexterity860-敏捷|dagger_damage2060-匕首傷害和移動速度|dagger_mobility2533-匕首的傷害和移動速度|from_the_shadows471-影刃|mastery_dagger78-匕首專精-44869|dexterity867-敏捷|life1415-生命|life_life_leech1629-嗜血者|mastery_life135-生命專精-47642|accuracy587-命中和暴擊率|accuracy586-命中和暴擊率|deadeye588-潛意識|mastery_accuracy2-命中專精-64775|intelligence927-智慧|intelligence926-智慧|dagger_damage264-匕首傷害|dagger_damage_and_critical_strike_chance465-匕首暴擊率和暴擊加成|dagger_damage_and_critical_strike_multiplier262-匕首暴擊率和加成|dagger_damage_and_critical_strike_multiplier263-匕首暴擊率和加成|dagger_global_crit_notable2527-背刺|dagger_damage_and_attack_speed466-匕首傷害和攻擊速度|dagger_damage_and_attack_speed467-匕首傷害和攻擊速度|dagger_damage_and_attack_speed468-匕首傷害和攻擊速度|flaying266-剝皮刃|intelligence1993-智慧|frenzy_charge_duration531-狂怒球持續時間|maximum_frenzy_charges526-狂熱|dexterity873-敏捷|dexterity858-敏捷|dexterity968-敏捷|dexterity835-敏捷|dexterity844-敏捷|dexterity840-敏捷|mana_on_kill1476-魔力和光環範圍效果|mana_on_kill_notable1704-狂歡|mastery_mana171-魔力專精-64875|dexterity838-敏捷|dexterity1990_-敏捷|maximum_frenzy_charges527-野性之心|strength1005-力量|life1213-生命|golem's_blood1088-魔像血統 |iron_reflexes1137-霸體|critical_strike_chance1019-暴擊率|assassination1239-刺殺|strength702-力量|channelling_damage2276-混沌抗性和避免|channelling_damage2277-混沌抗性|channelling_notabel2283-寬容|mark_generic1224-對標記敵人的傷害|mark_generic1225-對標記敵人的傷害|mark_generic_notable1226-標記獵物|elemental_damage2136-元素傷害|elemental_damage_notable2138-元素專注|dagger_damage_and_critical_strike_chance257-匕首暴擊加成|")
+SetNeedAddTianFu("游侠=evasion731-閃避和生命|")
 SetNeedAddTianFu("游侠升华=AscendancyDeadeye7-投射物傷害、投射物速度|AscendancyDeadeye8-狙擊|AscendancyDeadeye18-投射物傷害、投射物速度|AscendancyDeadeye6-無限彈藥|AscendancyDeadeye3-投射物傷害、攻擊速度|AscendancyDeadeye14-集風|") 
