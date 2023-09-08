7622df54384dedf861081d0f8aae0cb2aceb88c5
xlgj79718 <34760265+xlgj79718@users.noreply.github.com>
2023-09-08 17:23:50 +0800

    Delete TOOLBOX/右鍵關.bat

--- TOOLBOX/右鍵關.bat
+++
@@ -1,5 +0,0 @@
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoViewContextMenu" /d 1 /t REG_DWORD /f
-
-taskkill /f /im explorer.exe
-
-start explorer.exe
