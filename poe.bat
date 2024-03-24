aff22ffb57519d6d562218650eb25b37905d9018
xlgj79718 <34760265+xlgj79718@users.noreply.github.com>
2024-03-25 01:45:52 +0800

    Add files via upload

---
+++ poe.bat
@@ -0,0 +1,5 @@
+PUSHD %~DP0 & cd /d "%~dp0"
+%1 %2
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
+:runas
+start "" "D:\32808\poe.exe" --nologo --nopatch 
