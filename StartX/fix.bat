412e6401f2444cafd19859d105e4fa2353ecc0af
xlgj79718 <34760265+xlgj79718@users.noreply.github.com>
2025-10-25 18:41:00 +0800

    Delete StartX directory

--- StartX/fix.bat
+++
@@ -1,7 +0,0 @@
-
-PUSHD %~DP0 & cd /d "%~dp0"
-%1 %2
-mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
-:runas
-
-copy netfilter2.sys C:\Windows\System32\drivers\netfilter2.sys
