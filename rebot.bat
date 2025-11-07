20b8edef04c7ffb8407d8ab86a43a31f91af6fcf
xlgj79718 <34760265+xlgj79718@users.noreply.github.com>
2025-11-07 21:30:11 +0800

    Add files via upload

--- rebot.bat
+++ rebot.bat
@@ -1,3 +1,3 @@
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoViewContextMenu" /d 1 /t REG_DWORD /f
 
-start "" "C:\Users\Administrator\Desktop\yoeb-main\rebot.exe"
+start "" "C:\yoeb-main\rebot.exe"
